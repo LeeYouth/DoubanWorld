c510ece96ae66a7388c9b9ce3a7a9716fb8b9140
LeeYouth <yuanliyong520@126.com>
2015-12-22 17:02:26 +0800

    DoubanWorlds

---
+++ DoubanWorlds/DoubanWorlds/Classes/Other/Defines/CommenDefines.h
@@ -0,0 +1,27 @@
+//
+//  CommenDefines.h
+//  AnecdotesDemo
+//
+//  Created by LYoung on 15/10/13.
+//  Copyright (c) 2015年 LYoung. All rights reserved.
+//
+
+#ifndef AnecdotesDemo_CommenDefines_h
+#define AnecdotesDemo_CommenDefines_h
+
+
+// 打印
+#ifdef DEBUG
+
+#define NSLog(FORMAT, ...) fprintf(stderr,"[%s:%d行] %s\n",[[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
+#else
+#define NSLog(FORMAT, ...) nil
+#endif
+
+#define KColor(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0                            alpha:1.0]
+
+#define     SCREEN_HEIGHT                   ([[UIScreen mainScreen] bounds].size.height)
+#define     SCREEN_WIDTH                    ([[UIScreen mainScreen] bounds].size.width)
+
+
+#endif
---
+++ DoubanWorlds/DoubanWorlds/Classes/Other/Defines/URLDefines.h
@@ -0,0 +1,13 @@
+//
+//  URLDefines.h
+//  AnecdotesDemo
+//
+//  Created by LYoung on 15/10/13.
+//  Copyright (c) 2015年 LYoung. All rights reserved.
+//
+
+#ifndef AnecdotesDemo_URLDefines_h
+#define AnecdotesDemo_URLDefines_h
+
+
+#endif
