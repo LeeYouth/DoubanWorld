f77b80d5546e83feb4d4630039a442e52159ab25
LeeYouth <yuanliyong520@126.com>
2015-12-29 11:41:17 +0800

    DoubanWorlds commit

--- DoubanWorlds/DoubanWorlds/Classes/Recommend/Tool/LocationManager.m
+++ DoubanWorlds/DoubanWorlds/Classes/Recommend/Tool/LocationManager.m
@@ -105,6 +105,8 @@ static LocationManager  *manager;
             if ([cityName isEqualToString:@"不能发现城市"]) {
                 cityName = [cityUID stringByReplacingOccurrencesOfString:@"市" withString:@""];
             }
+            
+//            [[NSUserDefaults standardUserDefaults] setObject:<#(nullable id)#> forKey:<#(nonnull NSString *)#>]
 
             CLLocation *currentLocation = [dic objectForKey:@"location"];
             if (_locationBlock) {
