dafcd114a4b6994d190eae1cd20930feff91ad9a
LeeYouth <yuanliyong520@126.com>
2015-12-31 17:15:35 +0800

    DoubanWorlds commit

--- DoubanWorlds/DoubanWorlds/Classes/Recommend/Controller/RecommendViewController.m
+++ DoubanWorlds/DoubanWorlds/Classes/Recommend/Controller/RecommendViewController.m
@@ -348,14 +348,8 @@ static CGFloat randomFloatBetweenLowAndHigh(CGFloat low, CGFloat high) {
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)dealloc{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
-*/
 
 @end
